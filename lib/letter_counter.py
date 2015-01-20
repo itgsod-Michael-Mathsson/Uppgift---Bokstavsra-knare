892639ef9f8fd99cbf31cb935dfeffcca21b1f7b
itgsod-Michael-Mathsson <michael.mathsson@elev.it-gymnasiet.se>
2015-01-20 14:11:15 +0100

    I've made the letter counter

---
+++ lib/letter_counter.py
@@ -0,0 +1,20 @@
+__author__ = 'michael.mathsson'
+
+import string
+
+
+def count_letters(text):
+    letters = {}
+
+    for letter in text.lower():
+        if letter in string.letters:
+            letters[letter] = 0
+    for letter in text.lower():
+        if letter in string.letters:
+            letters[letter] +=1
+
+    print letters
+    return letters
+
+
+count_letters("hello")
